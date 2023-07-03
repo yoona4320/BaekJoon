229752112222b3a91cc3fc7ae2a5e6a7be63b3bf
yoona4320 <yoona4320@naver.com>
2023-07-03 21:49:30 +0900

    test3

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("test2");
+        System.out.println("test3");
     }    
 }
